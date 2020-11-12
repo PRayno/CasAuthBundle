8557b24c1348dfa6fea47d1432bc25e90a69e79f
Fred <frederic.casazza@univ-cotedazur.fr>
2020-11-12 11:23:13 +0100

    Fix use statement in CasUser

--- Security/User/CasUserProvider.php
+++ Security/User/CasUserProvider.php
@@ -2,7 +2,6 @@
 // src/AppBundle/Security/User/CasProvider.php
 namespace PRayno\CasAuthBundle\Security\User;
 
-use Security\User\CasUserProviderInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
